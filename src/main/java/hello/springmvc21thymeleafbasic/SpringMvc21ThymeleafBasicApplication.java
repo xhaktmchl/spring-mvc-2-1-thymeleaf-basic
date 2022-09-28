57435c23914168d76fd93cf2c7106213a1db70b9
xhaktmchl <xhaktmchl@naver.com>
2022-09-28 22:01:09 +0900

    chore: 프로젝트 빌드 및 웰컴 index.html페이지 작성

---
+++ src/main/java/hello/springmvc21thymeleafbasic/SpringMvc21ThymeleafBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.springmvc21thymeleafbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvc21ThymeleafBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvc21ThymeleafBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/springmvc21thymeleafbasic/SpringMvc21ThymeleafBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springmvc21thymeleafbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc21ThymeleafBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
